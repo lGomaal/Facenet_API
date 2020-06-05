d9f5c620c6926208a6e28a69778bf24337305002
Mohamed Gomaa <mohamed.gomaa@cis.asu.edu.eg>
2020-06-05 03:19:01 +0200

    third

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
+
+from flask import Flask, request, jsonify, render_template
 import numpy as np
 import cv2
 from scipy.spatial import distance
-from keras.models import load_model
 from skimage.transform import resize
-from keras import backend as k
+from keras.models import load_model
+import tensorflow as tf
 
+app = Flask(__name__)
+global graph
+graph = tf.get_default_graph()
 image_size = 160
-cascade_path = 'app/modelFiles/haarcascade_frontalface_alt2.xml'
-model_path = 'app/modelFiles/facenet_keras.h5'
+cascade_path = 'haarcascade_frontalface_alt2.xml'
+model_path = 'facenet_keras.h5'
+model = load_model(model_path)
+
+
+
 
 
 
@@ -65,3 +74,34 @@ def get_diff(img1, model, graph):
     embs2 = encoding(image2, model, graph)
     # k.clear_session()
     return distance.euclidean(embs1, embs2)
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+
+    r = request
+    # convert string of image data to uint8
+    nparr = np.fromstring(r.data, np.uint8)
+    # decode image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    # print(img.shape)
+    #
+    # response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
+    #             }
+    # # encode response using jsonpickle
+    # response_pickled = jsonpickle.encode(response)
+
+    # return Response(response=response_pickled, status=200, mimetype="application/json")
+    return jsonify(get_diff(img, model, graph))
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
